596dcbed65f0b973624569bf4e2b699466995cda
Sanmimimi <3864620423@qq.com>
2025-08-22 13:29:21 +0800

    Update social.ts

--- data/social.ts
+++ data/social.ts
@@ -25,7 +25,7 @@ const social: Social = {
   x: 'https://twitter.com/kuizuo',
   juejin: 'https://juejin.cn/user/1565318510545901',
   wx: 'https://img.kuizuo.me/wechat.png',
-  // qq: 'https://img.kuizuo.me/qq.png',
+  qq: 'https://img.kuizuo.me/qq.png',
   zhihu: 'https://www.zhihu.com/people/kuizuo',
   cloudmusic: 'https://music.163.com/#/user/home?id=1333010742',
   email: 'mailto:hi@kuizuo.me',
