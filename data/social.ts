6f9019efcbc1319afb46bfbeb87ba1c5d4624405
Sanmimimi <3864620423@qq.com>
2025-08-22 17:05:33 +0800

    Update social.ts

--- data/social.ts
+++ data/social.ts
@@ -8,6 +8,10 @@ export type Social = {
   zhihu?: string
   email?: string
   discord?: string
+  douyin?: string
+  weibo?: string
+  bilibili?: string
+  xiaohongshu?: string
 }
 
 type SocialValue = {
@@ -27,26 +31,36 @@ const social: Social = {
   cloudmusic: 'https://music.163.com',
   email: 'mailto:hi@kuizuo.me',
   discord: 'https://discord.gg/M8cVcjDxkz',
+  douyin: 'https://www.douyin.com',
+  weibo: 'https://weibo.com',
+  bilibili: 'https://space.bilibili.com',
+  xiaohongshu: 'https://www.xiaohongshu.com',
 }
 
 const socialSet: Record<keyof Social | 'rss', SocialValue> = {
-  github: {
-    href: social.github,
-    title: 'GitHub',
-    icon: 'ri:github-line',
-    color: '#010409',
+   weibo: {
+    href: social.weibo,
+    title: '微博',
+    icon: 'ri:weibo-line',
+    color: '#E6162D',
   },
-  juejin: {
-    href: social.juejin,
-    title: '掘金',
-    icon: 'simple-icons:juejin',
-    color: '#1E81FF',
+  ilibili: {
+    href: social.bilibili,
+    title: '哔哩哔哩',
+    icon: 'ri:bilibili-line',
+    color: '#00AEEC',
   },
-  x: {
-    href: social.x,
-    title: 'X',
-    icon: 'ri:twitter-x-line',
-    color: '#000',
+  douyin: {
+    href: social.douyin,
+    title: '抖音',
+    icon: 'simple-icons:tiktok',
+    color: '#FF0050',    
+  },
+  xiaohongshu: {
+    href: social.xiaohongshu,
+    title: '小红书',
+    icon: 'ri:book-line',
+    color: '#FF2442',
   },
   wx: {
     href: social.wx,
@@ -54,29 +68,17 @@ const socialSet: Record<keyof Social | 'rss', SocialValue> = {
     icon: 'ri:wechat-2-line',
     color: '#07c160',
   },
-  zhihu: {
-    href: social.zhihu,
-    title: '知乎',
-    icon: 'ri:zhihu-line',
-    color: '#1772F6',
-  },
-  discord: {
-    href: social.discord,
-    title: 'Discord',
-    icon: 'ri:discord-line',
-    color: '#5A65F6',
-  },
   qq: {
     href: social.qq,
     title: 'QQ',
     icon: 'ri:qq-line',
     color: '#1296db',
   },
-  email: {
-    href: social.email,
-    title: '邮箱',
-    icon: 'ri:mail-line',
-    color: '#D44638',
+  zhihu: {
+    href: social.zhihu,
+    title: '知乎',
+    icon: 'ri:zhihu-line',
+    color: '#1772F6',
   },
   cloudmusic: {
     href: social.cloudmusic,
@@ -84,6 +86,36 @@ const socialSet: Record<keyof Social | 'rss', SocialValue> = {
     icon: 'ri:netease-cloud-music-line',
     color: '#C20C0C',
   },
+  juejin: {
+    href: social.juejin,
+    title: '掘金',
+    icon: 'simple-icons:juejin',
+    color: '#1E81FF',  
+  },
+  email: {
+    href: social.email,
+    title: '邮箱',
+    icon: 'ri:mail-line',
+    color: '#D44638',
+ },
+  github: {
+    href: social.github,
+    title: 'GitHub',
+    icon: 'ri:github-line',
+    color: '#010409',
+ },
+  x: {
+    href: social.x,
+    title: 'X',
+    icon: 'ri:twitter-x-line',
+    color: '#000',   
+ },
+  discord: {
+    href: social.discord,
+    title: 'Discord',
+    icon: 'ri:discord-line',
+    color: '#5A65F6',  
+ },
   rss: {
     href: '/blog/rss.xml',
     title: 'RSS',
