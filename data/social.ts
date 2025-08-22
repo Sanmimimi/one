6590c008cae5101d9c30249a32be81a1bbfa9584
Sanmimimi <3864620423@qq.com>
2025-08-22 16:53:32 +0800

    Update social.ts

--- data/social.ts
+++ data/social.ts
@@ -1,12 +1,7 @@
 export type Social = {
   github?: string
   x?: string
-  douyin?: string
-  weibo?: string
-  bilibili?: string
-  xiaohongshu?: string
   juejin?: string
-  qmusic?: string
   qq?: string
   wx?: string
   cloudmusic?: string
@@ -26,73 +21,20 @@ const social: Social = {
   github: 'https://github.com/kuizuo',
   x: 'https://twitter.com/kuizuo',
   juejin: 'https://juejin.cn/user/1565318510545901',
-  douyin: 'https://www.douyin.com',
-  weibo: 'https://weibo.com',
-  bilibili: 'https://space.bilibili.com'
-  xiaohongshu: 'https://www.xiaohongshu.com',  
   wx: 'https://img.kuizuo.me/wechat.png',
   // qq: 'https://img.kuizuo.me/qq.png',
-  // qqmusic: 'https://y.qq.com',   
   zhihu: 'https://www.zhihu.com/people/kuizuo',
-  cloudmusic: 'https://music.163.com/#/user/home?id=1333010742',
+  cloudmusic: 'https://music.163.com',
   email: 'mailto:hi@kuizuo.me',
   discord: 'https://discord.gg/M8cVcjDxkz',
 }
 
 const socialSet: Record<keyof Social | 'rss', SocialValue> = {
-  weibo: {
-    href: social.weibo,
-    title: '微博',
-    icon: 'ri:weibo-line',
-    color: '#E6162D',
-  },
-  bilibili: {
-    href: social.bilibili,
-    title: '哔哩哔哩',
-    icon: 'ri:bilibili-line',
-    color: '#00AEEC',
-  },
-  douyin: {
-    href: social.douyin,
-    title: '抖音',
-    icon: 'simple-icons:tiktok',
-    color: '#FF0050',    
-  },
-  xiaohongshu: {
-    href: social.xiaohongshu,
-    title: '小红书',
-    icon: 'ri:book-line',
-    color: '#FF2442',
-  },
-  wx: {
-    href: social.wx,
-    title: '微信',
-    icon: 'ri:wechat-2-line',
-    color: '#07c160',
-  },
-  qq: {
-    href: social.qq,
-    title: 'QQ',
-    icon: 'ri:qq-line',
-    color: '#1296db',
-  },
-  zhihu: {
-    href: social.zhihu,
-    title: '知乎',
-    icon: 'ri:zhihu-line',
-    color: '#1772F6', 
-  },
-  qqmusic: {
-    href: social.qqmusic,
-    title: 'QQ音乐',
-    icon: 'ri:music-line',
-    color: '#31C27C',  
-  },
-   cloudmusic: {
-    href: social.cloudmusic,
-    title: '网易云',
-    icon: 'ri:netease-cloud-music-line',
-    color: '#C20C0C',
+  github: {
+    href: social.github,
+    title: 'GitHub',
+    icon: 'ri:github-line',
+    color: '#010409',
   },
   juejin: {
     href: social.juejin,
@@ -100,29 +42,47 @@ const socialSet: Record<keyof Social | 'rss', SocialValue> = {
     icon: 'simple-icons:juejin',
     color: '#1E81FF',
   },
-  email: {
-    href: social.email,
-    title: '邮箱',
-    icon: 'ri:mail-line',
-    color: '#D44638',
-  },
-  github: {
-    href: social.github,
-    title: 'GitHub',
-    icon: 'ri:github-line',
-    color: '#010409',
-  },
   x: {
     href: social.x,
     title: 'X',
     icon: 'ri:twitter-x-line',
     color: '#000',
   },
+  wx: {
+    href: social.wx,
+    title: '微信',
+    icon: 'ri:wechat-2-line',
+    color: '#07c160',
+  },
+  zhihu: {
+    href: social.zhihu,
+    title: '知乎',
+    icon: 'ri:zhihu-line',
+    color: '#1772F6',
+  },
   discord: {
     href: social.discord,
     title: 'Discord',
     icon: 'ri:discord-line',
-    color: '#5A65F6',  
+    color: '#5A65F6',
+  },
+  qq: {
+    href: social.qq,
+    title: 'QQ',
+    icon: 'ri:qq-line',
+    color: '#1296db',
+  },
+  email: {
+    href: social.email,
+    title: '邮箱',
+    icon: 'ri:mail-line',
+    color: '#D44638',
+  },
+  cloudmusic: {
+    href: social.cloudmusic,
+    title: '网易云',
+    icon: 'ri:netease-cloud-music-line',
+    color: '#C20C0C',
   },
   rss: {
     href: '/blog/rss.xml',
