f3c503b38e27869aa9712dc5e67f3bbc3a21f5cb
Sanmimimi <3864620423@qq.com>
2025-08-23 22:38:18 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -115,7 +115,7 @@ const config: Config = {
         <p style="display: inline-flex; align-items: center;"><img style="height:20px;margin-right: 0.5rem;" src="/img/police.png" alt="police" height="20"/><a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=${
           beian1.match(/\d+/)?.[0]
         }" >${beian1}</a></p>
-        <p>Copyright © 2020 - ${new Date().getFullYear()} <a href="/" style="color: #1976d2; text-decoration: none;">美若黎明</a>. All Rights Reserved</p>
+        <p>Copyright © 2020 - ${new Date().getFullYear()} <a href="/" style="color:rgb(76, 192, 228); text-decoration: none;">美若黎明</a>. All Rights Reserved</p>
         `,
     },
     algolia: {
