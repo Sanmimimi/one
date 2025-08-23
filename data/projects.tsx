a17407d5625f5c24b6b66bd7fb67bdf4c9c126f1
Sanmimimi <3864620423@qq.com>
2025-08-23 20:32:21 +0800

    Update projects.tsx

--- data/projects.tsx
+++ data/projects.tsx
@@ -1,6 +1,6 @@
 export const projects: Project[] = [
   {
-    title: '愧怍的小站',
+    title: '我的小站',
     description: '🦖 基于 Docusaurus 静态网站生成器实现个人博客',
     preview: '/img/project/blog.png',
     website: 'https://kuizuo.me',
