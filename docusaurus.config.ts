bd21b26c0f7d339f16e1d91cdc60d9cb1c35327f
Sanmimimi <3864620423@qq.com>
2025-09-20 20:33:59 +0800

    Add files via upload

---
+++ docusaurus.config.ts
@@ -0,0 +1,302 @@
+import type * as Preset from '@docusaurus/preset-classic'
+import type { Config } from '@docusaurus/types'
+import { themes } from 'prism-react-renderer'
+import social from './data/social'
+import type { GiscusConfig } from './src/components/Comment'
+
+const beian = '京ICP备15006448号-3'
+const beian1 = '京公网安备110402430053号'
+
+const config: Config = {
+  title: '言兑林夕',
+  url: 'https://kuizuo.me',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'kuizuo',
+  projectName: 'blog',
+  customFields: {
+    bio: '少年当有鸿鹄志，曾许人间第一流',
+    description:
+      '是一个由我创建的个人空间，主要分享编程开发知识和项目。',
+  },
+  themeConfig: {
+    // announcementBar: {
+    //   id: 'announcementBar-3',
+    //   content: ``,
+    // },
+    image: 'img/og.png',
+    metadata: [
+      {
+        name: 'author',
+        content: '愧怍',
+      },
+      {
+        name: 'keywords',
+        content: 'blog, javascript, typescript, node, react, vue, web',
+      },
+      {
+        name: 'keywords',
+        content: '编程爱好者, Web开发者, 写过爬虫, 学过逆向, 主攻ts全栈',
+      },
+    ],
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
+    navbar: {
+      logo: {
+        alt: '我',
+        src: 'img/logo.webp',
+        srcDark: 'img/logo.webp',
+      },
+      hideOnScroll: true,
+      items: [
+        { label: '空间', position: 'right', to: 'blog' },
+        { label: '项目', position: 'right', to: 'project' },
+        { label: '友链', position: 'right', to: 'friends' },
+        { label: '关于', position: 'right', to: 'about' },
+        {
+          label: '更多',
+          position: 'right',
+          items: [
+            { label: '归档', to: 'blog/archive' },
+            { label: '标签', to: 'blog/tags' },
+          ],
+        },
+        // {
+        //   type: 'localeDropdown',
+        //   position: 'right',
+        // },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: '学习',
+          items: [
+            { label: '空间', to: 'blog' },
+            { label: '归档', to: 'blog/archive' },
+            { label: '标签', to: 'blog/tags' },
+            { label: '实战项目', to: 'project' },
+            { label: '前端示例', to: 'https://www.douyin.com/' },
+          ],
+        },
+        {
+          title: '社交媒体',
+          items: [
+            { label: '关于我', to: '/about' },
+            { label: 'GitHub', href: social.github.href },
+            { label: 'X', href: social.x.href },
+            { label: '掘金', href: social.juejin.href },
+            { label: 'Discord', href: social.discord.href },
+          ],
+        },
+        {
+          title: '网站',
+          items: [
+            { label: '哔哩哔哩', to: 'https://www.bilibili.com' },
+            { label: 'cyberChef', to: 'https://gchq.github.io/CyberChef' },
+            { label: '微博', to: 'https://www.weibo.com' },
+            { label: '抖音', to: 'https://www.douyin.com' },
+            { label: '高考倒计时', to: 'https://a.shxy.dpdns.org/353535.html' },
+          ],
+        },
+        {
+          title: '更多',
+          items: [
+            { label: '友链', position: 'right', to: 'friends' },
+          ],
+        },
+      ],
+      copyright: `
+        <p style="margin-bottom: 0;"><a href="http://beian.miit.gov.cn/">${beian}</a></p>
+        <p style="display: inline-flex; align-items: center;"><img style="height:20px;margin-right: 0.5rem;" src="/img/police.png" alt="police" height="20"/><a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=${
+          beian1.match(/\d+/)?.[0]
+        }" >${beian1}</a></p>
+        <p>Copyright © 2020 - ${new Date().getFullYear()} <a href="/" style="color:rgb(255, 255, 255); text-decoration: none;">言兑林夕</a>. All Rights Reserved</p>
+        `,
+    },
+    algolia: {
+      appId: 'GV6YN1ODMO',
+      apiKey: '50303937b0e4630bec4a20a14e3b7872',
+      indexName: 'kuizuo',
+    },
+    prism: {
+      theme: themes.oneLight,
+      darkTheme: themes.oneDark,
+      additionalLanguages: ['bash', 'json', 'java', 'python', 'php', 'graphql', 'rust', 'toml', 'protobuf', 'diff'],
+      defaultLanguage: 'javascript',
+      magicComments: [
+        {
+          className: 'theme-code-block-highlighted-line',
+          line: 'highlight-next-line',
+          block: { start: 'highlight-start', end: 'highlight-end' },
+        },
+        {
+          className: 'code-block-error-line',
+          line: 'This will error',
+        },
+      ],
+    },
+    giscus: {
+      repo: 'Sanmimimi/pinlun',
+      repoId: 'R_kgDOPhhjUg',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOPhhjUs4CuaF4',
+      theme: 'light',
+      darkTheme: 'dark_dimmed',
+    } satisfies Partial<GiscusConfig>,
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 4,
+    },
+    liveCodeBlock: { playgroundPosition: 'top' },
+    zoom: {
+      selector: '.markdown :not(em) > img',
+      background: {
+        light: 'rgb(255, 255, 255)',
+        dark: 'rgb(50, 50, 50)',
+      },
+    },
+  } satisfies Preset.ThemeConfig,
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          path: 'docs',
+          sidebarPath: 'sidebars.ts',
+        },
+        blog: false,
+        theme: {
+          customCss: ['./src/css/custom.css', './src/css/tweet-theme.css'],
+        },
+        sitemap: {
+          priority: 0.5,
+        },
+        gtag: {
+          trackingID: 'G-S4SD5NXWXF',
+          anonymizeIP: true,
+        },
+        debug: process.env.NODE_ENV === 'development',
+      } satisfies Preset.Options,
+    ],
+  ],
+  plugins: [
+    'docusaurus-plugin-image-zoom',
+    '@docusaurus/plugin-ideal-image',
+    // ['docusaurus-plugin-baidu-tongji', { token: 'c9a3849aa75f9c4a4e65f846cd1a5155' }],
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: process.env.NODE_ENV === 'development',
+        offlineModeActivationStrategies: ['appInstalled', 'standalone', 'queryString'],
+        pwaHead: [
+          { tagName: 'link', rel: 'icon', href: '/img/logo.png' },
+          { tagName: 'link', rel: 'manifest', href: '/manifest.json' },
+          { tagName: 'meta', name: 'theme-color', content: '#12affa' },
+        ],
+      },
+    ],
+    [
+      'vercel-analytics',
+      {
+        debug: process.env.NODE_ENV === 'development',
+        mode: 'auto',
+      },
+    ],
+    [
+      './src/plugin/plugin-content-blog', // 为了实现全局 blog 数据，必须改写 plugin-content-blog 插件
+      {
+        path: 'blog',
+        editUrl: ({ locale, blogDirPath, blogPath, permalink }) =>
+          `https://github.com/kuizuo/blog/edit/main/${blogDirPath}/${blogPath}`,
+        editLocalizedFiles: false,
+        blogDescription: '代码人生：编织技术与生活的空间之旅',
+        blogSidebarCount: 10,
+        blogSidebarTitle: '空间',
+        postsPerPage: 12,
+        showReadingTime: true,
+        readingTime: ({ content, frontMatter, defaultReadingTime }) =>
+          defaultReadingTime({ content, options: { wordsPerMinute: 300 } }),
+        feedOptions: {
+          type: 'all',
+          title: '我',
+          description: 'feedId:41215011978385457+userId:41840354283324416',
+          copyright: `Copyright © ${new Date().getFullYear()} 言兑林夕. All Rights Reserved.<p><a href="http://beian.miit.gov.cn/" class="footer_lin">${beian}</a></p>`,
+        },
+      },
+    ],
+    async function tailwindcssPlugin() {
+      return {
+        name: 'docusaurus-tailwindcss',
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require('tailwindcss'))
+          postcssOptions.plugins.push(require('autoprefixer'))
+          return postcssOptions
+        },
+      }
+    },
+    async function injectMotto() {
+      return {
+        name: 'docusaurus-motto',
+        injectHtmlTags() {
+          return {
+            headTags: [
+              {
+                tagName: 'script',
+                innerHTML: `
+    (${function () {
+      console.log(
+        `%c Kz Blog %c https://github.com/kuizuo/blog`,
+        'color: #fff; margin: 1em 0; padding: 5px 0; background: #12affa;',
+        'margin: 1em 0; padding: 5px 0; background: #efefef;',
+      )
+
+      const motto = `
+This Webisite Powered By Kz Blog.
+Written by Docusaurus, Coding with Love.
+--------
+Love what you do and do what you love.
+`
+
+      if (document.firstChild?.nodeType !== Node.COMMENT_NODE) {
+        document.prepend(document.createComment(motto))
+      }
+    }.toString()})();`,
+              },
+            ],
+          }
+        },
+      }
+    },
+  ],
+  headTags: [
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'description',
+        content: '我的个人空间',
+      },
+    },
+  ],
+  stylesheets: [
+    'https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Normal.min.css',
+    'https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Medium.min.css',
+    'https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Semibold.min.css',
+  ],
+  i18n: {
+    defaultLocale: 'zh-CN',
+    locales: ['zh-CN'],
+  },
+  onBrokenLinks: 'warn',
+  future: {
+    v4: true,
+    experimental_faster: true,
+  },
+}
+
+export default config
